e069928bae11b58539ab4d09129c7fc1f62ee92a
yterajima <terra@e2esound.com>
2015-01-22 21:43:13 +0900

    related #1: Add command class and Update test

--- features/tansu_cli.feature
+++ features/tansu_cli.feature
@@ -6,6 +6,16 @@ Feature: Tansu Command
     Then the exit status should be 0
     And a file named "source/foo.html.md" should exist
     And the file "source/foo.html.md" should contain "title: foo"
+    And the file "source/foo.html.md" should not contain "title: foo.html.md"
     And the file "source/foo.html.md" should contain "author:"
     And the file "source/foo.html.md" should contain "date:"
 
+  Scenario: middleman tansu create a markdown file that has subdirectory
+    Given a fixture app "empty-app"
+    When I run `middleman tansu foo/bar/baz`
+    Then the exit status should be 0
+    And a file named "source/foo/bar/baz.html.md" should exist
+    And the file "source/foo/bar/baz.html.md" should contain "title: baz"
+    And the file "source/foo/bar/baz.html.md" should not contain "title: foo/bar/baz"
+    And the file "source/foo/bar/baz.html.md" should contain "author:"
+    And the file "source/foo/bar/baz.html.md" should contain "date:"
--- lib/middleman-tansu/command.rb
+++ lib/middleman-tansu/command.rb
@@ -1,5 +1,73 @@
 module Middleman
   module Tansu
+    # This class provides a "tansu" command for the middleman CLI.
+    class Tansu < Thor
+      include Thor::Actions
 
+      check_unknown_options!
+
+      namespace :tansu
+
+      def self.source_root
+        ENV['MM_ROOT']
+      end
+
+      # Tell Thor to exit with a nonzero exit code on failure
+      def self.exit_on_failure?
+        true
+      end
+
+      desc "tansu path/to/TITLE", "Create a new Tansu page with markdown"
+      def tansu(path)
+        paths = path.split("/")
+        title = paths.pop
+
+        if /\.html\.md$/ !~ title
+          filename = title + '.html.md'
+        end
+
+        dir  = destination_dir(paths)
+        file = File.join(dir, filename)
+
+        if !Dir.exists?(dir)
+          FileUtils.mkdir_p dir
+        end
+
+        if File.exists?(file)
+          puts "#{file} is exist"
+          exit
+        end
+
+        File.open(file, 'w') do |f|
+          f.puts frontmatter(title)
+        end
+        puts "create new article: #{file}"
+      end
+
+      no_tasks do
+        def frontmatter(title)
+          rows = []
+          rows << "---"
+          rows << "title: #{title}"
+          rows << "author: #{ENV['USER']}"
+          rows << "date: #{Time.now}"
+          rows << "---"
+          rows << "\n\n"
+
+          rows.join("\n")
+        end
+
+        def destination_dir(dir)
+          app    = Middleman::Application
+          source = File.join(app.root, app.config.source)
+
+          if dir == nil || dir == '.'
+            source
+          else
+            File.join(source, dir)
+          end
+        end
+      end
+    end
   end
 end
