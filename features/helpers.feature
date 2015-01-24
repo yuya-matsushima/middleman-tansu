8628bce7af5fafe2b3a7a2ca7b920a5a6572eae0
yterajima <terra@e2esound.com>
2015-01-24 21:54:29 +0900

    Add path_list helper

--- features/helpers.feature
+++ features/helpers.feature
@@ -1,7 +1,10 @@
 Feature: Helpers
 
-  Scenario: `page_list` helper
-    Given a fixture app "page-list-app"
+  Scenario: `path_list` helper
+    Given a fixture app "helper-app"
     When I run `middleman build`
     Then the exit status should be 0
+    And the file "build/index.html" should contain "No Path List"
+    And the file "build/page/list/index.html" should contain "<li>page:/page/</li>"
+    And the file "build/page/list/index.html" should contain "<li>list:/page/list/</li>"
 
--- lib/middleman-tansu/helpers.rb
+++ lib/middleman-tansu/helpers.rb
@@ -1,7 +1,16 @@
 module Middleman
   module Tansu
     module Helpers
-      def page_list
+      def path_list(current_path)
+        paths = []
+        splited = current_path.split("/")
+        splited.each_with_index do |val, index|
+          path = File.join("/", splited[0..index])
+          path = path + "/" unless /\.html$/ =~ path
+          val  = val.sub(".html", "") if /\.html$/ =~ val
+          paths.push({path: path, name: val}) unless /^index$/ =~ val
+        end
+        paths
       end
     end
   end
